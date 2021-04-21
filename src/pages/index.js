0cf3da2472f4442d436043eaa92653caadadafed
Nabin Purja <nabinpurja93@gmail.com>
2021-04-21 13:15:34 +0100

    component and page set up for the portra project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import TitleBlock from '../components/TitleBlock';
 
 export default function Home() {
     return (
         <>
             <TitleBlock />
+            <Link to="/portra">Portra project</Link>
         </>
     );
 }
---
+++ src/pages/portra.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portra from '../components/Portra';
+
+const PortraPage = () => {
+    return (
+        <div>
+            <Portra />
+        </div>
+    );
+};
+
+export default PortraPage;
