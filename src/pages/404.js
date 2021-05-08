58c9ab87eafa42f0279fb5f494d10adae4dacde1
Nabin Purja <nabinpurja93@gmail.com>
2021-05-08 11:43:50 +0100

    Created a basic 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,16 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import SEO from '../components/SEO';
 
-const PageNotFound = () => {
-    return (
-        <div>
-            <h1>404</h1>
-            <h2>Page not found</h2>
-        </div>
-    );
-};
+const PageNotFound = () => (
+    <div className="h-screen container mx-auto text-center flex flex-col justify-center">
+        <SEO title="404 - Page not found!" />
+        <h1>404</h1>
+        <p className="uppercase">Page not found!</p>
+        <Link to="/" className="underline italic my-4 inline-block">
+            Return to home
+        </Link>
+    </div>
+);
 
 export default PageNotFound;
